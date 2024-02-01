473e39a855aa4468e520b0d795d88ed32aa6f3c4
p6nj <breee@duck.com>
2024-02-01 20:20:14 +0100

    new cli interface for multiple todo lists

--- src/toudou.py
+++ src/toudou.py
@@ -9,11 +9,15 @@ from dataclasses import dataclass
 l = list
 
 
-@dataclass
 class TodoList:
+    def __init__(self) -> None:
+        self.items: [TodoList.Item] = []
+
+    def __str__(self) -> str:
+        return linesep.join([str(t) for t in self.items])
+
     @dataclass
-    class Todo:
-        id: int
+    class Item:
         task: str
         date: date | None
         done: bool
@@ -25,55 +29,39 @@ class TodoList:
                 + (" (" + self.date + ")" if self.date is not None else "")
             )
 
-    # use the next line for multiple todo lists
-    # id: ID
-    file: str
-
-    def exists(s) -> bool:
-        return isfile(s.file)
-
-    def empty(s) -> bool:
-        with open(s.file, "br") as file:
-            return not file.read()
-
-    def new(s, task: str, date=None):
-        if not s.exists:
-            with open(s.file, "x+b") as file:
-                dump([TodoList.Todo(1, task, date, False)], file)
-        else:
-            if s.empty:
-                with open(s.file, "bw") as file:
-                    dump([TodoList.Todo(1, task, date, False)], file)
-            else:
-                with open(s.file, "br") as file:
-                    todolist = load(file)
-                with open(s.file, "bw") as file:
-                    dump(
-                        todolist + [TodoList.Todo(len(todolist), task, date, False)],
-                        file,
-                    )
-
-    def __str__(s) -> str:
-        if not s.exists:
-            return "No todolist!"
-        if s.empty:
-            return "Nothing to do."
-        with open(s.file, "br") as file:
-            return linesep.join([str(t) for t in load(file)])
-
 
 @click.group()
 def cli():
     pass
 
 
-@cli.command()
-@click.option("-t", "--task", prompt="Your task", help="The task to remember.")
+@cli.group()
+def lists():
+    pass
+
+
+@cli.group()
+def new():
+    pass
+
+
+@new.command()
+@click.argument("name", type=click.File("xb"))
+def list(name):
+    dump(TodoList(), name)
+
+
+@new.command()
+@click.argument("task")
+@click.option(
+    "-l",
+    "--list",
+    type=click.File("rb+"),
+    prompt="Todo-list",
+    help="The todo-list where to put this task.",
+)
 @click.option("-d", "--duefor", help="The date the task is due for.")
-def new(task: str, duefor=None):
-    TodoList("todolist.p").new(task, duefor)
-
-
-@cli.command()
-def list():
-    print(TodoList("todolist.p"))
+def todo(task: str, list, duefor=None):
+    todos = load(list)
+    todos.items.append(TodoList.Item(task, duefor, False))
+    dump(todos, list)
