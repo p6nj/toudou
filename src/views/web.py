4fa8f23418c6aef6c1064ea4a592d69e6832b4e9
p6nj <breee@duck.com>
2024-03-09 00:17:07 +0100

    from lists to nav, favicon

--- src/views/web.py
+++ src/views/web.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, send_file
 from models import List, Session
 
 app = Flask(__name__)
@@ -6,10 +6,20 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
+    return render_template("home.html", nav=nav())
+
+
+@app.route("/nav")
+def nav():
     with Session() as session:
         return render_template(
-            "home.html", lists=[list.name for list in session.query(List).all()]
+            "nav.htm", lists=[list.name for list in session.query(List).all()]
         )
+
+
+@app.route("/favicon.ico")
+def favicon():
+    return send_file("static/favicon.ico")
 
 
 # static files
