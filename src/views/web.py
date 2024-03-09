bf2fb36c19d988a6df70a1d3656d8f6169a23f9f
p6nj <breee@duck.com>
2024-03-09 10:56:16 +0100

    better names, increase header modularity

--- src/views/web.py
+++ src/views/web.py
@@ -6,17 +6,27 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return render_template("home.html", nav=nav())
+    return render_template("index.html", header=header(), main=home())
 
 
-@app.route("/nav")
-def nav():
+@app.route("/header/<int:new>/<current>")
+def header(*, new: bool = False, current: str = None):
     with Session() as session:
         return render_template(
-            "nav.htm", lists=[list.name for list in session.query(List).all()]
+            "header.htm",
+            lists=[list.name for list in session.query(List).all()],
+            new=new,
+            current=current,
         )
 
 
+# main fillers
+@app.route("/home")
+def home():
+    return render_template("home.htm")
+
+
+# misc
 @app.route("/favicon.ico")
 def favicon():
     return send_file("static/favicon.ico")
